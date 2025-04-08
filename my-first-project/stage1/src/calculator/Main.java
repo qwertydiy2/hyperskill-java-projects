5a47697849ef29ec6f8967958dea74b035be6cb7
qwertydiy2 <107488390+qwertydiy2@users.noreply.github.com>
2025-04-08 13:38:02 +0100

    feat(calculator): add initial price list display in Main

---
+++ my-first-project/stage1/src/calculator/Main.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Prices:\nBubblegum: $2\nToffee: $0.2\nIce cream: $5\nMilk chocolate: $4\nDoughnut: $2.5\nPancake: $3.2");
+    }
+}
